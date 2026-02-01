6140626a98d879fcfa91b496c10690b20db1ab94
TimChen <37008451+TimChen1383@users.noreply.github.com>
2026-02-02 00:30:29 +0800

    add render guassian splatting render feature

--- Plugins/GaussianSplatting/Source/GaussianSplatting/GaussianSplatting.Build.cs
+++ Plugins/GaussianSplatting/Source/GaussianSplatting/GaussianSplatting.Build.cs
@@ -7,47 +7,42 @@ public class GaussianSplatting : ModuleRules
 	public GaussianSplatting(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
-		
+
 		PublicIncludePaths.AddRange(
 			new string[] {
-				// ... add public include paths required here ...
 			}
-			);
-				
-		
+		);
+
 		PrivateIncludePaths.AddRange(
 			new string[] {
-				// ... add other private include paths required here ...
 			}
-			);
-			
-		
+		);
+
 		PublicDependencyModuleNames.AddRange(
 			new string[]
 			{
 				"Core",
-				// ... add other public dependencies that you statically link with here ...
+				"CoreUObject",
+				"Engine",
+				"RenderCore",
+				"RHI",
+				"Renderer",
+				"Projects"
 			}
-			);
-			
-		
+		);
+
 		PrivateDependencyModuleNames.AddRange(
 			new string[]
 			{
-				"CoreUObject",
-				"Engine",
 				"Slate",
-				"SlateCore",
-				// ... add private dependencies that you statically link with here ...	
+				"SlateCore"
 			}
-			);
-		
-		
+		);
+
 		DynamicallyLoadedModuleNames.AddRange(
 			new string[]
 			{
-				// ... add any modules that your module loads dynamically here ...
 			}
-			);
+		);
 	}
 }
